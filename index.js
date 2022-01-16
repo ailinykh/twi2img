7a73ce94869c2823fcc0c3a5455de15922cde2f7
Anthony Ilinykh <anthonyilinykh@gmail.com>
2022-01-16 19:42:09 +0300

    Start amqp right after koa app started

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ require('dotenv').config()
 
 const app = require('./app')
 const config = require('./lib/config')
+const run = require('./lib/amqp')
+const createImage = require('./image/image-factory')
+const makeFile = require('./image/image-puppeteer')
 
 app.listen(config.port, () => {
   console.info(`App is running at http://${config.host}:${config.port}`)
+
+  run({ handleMessage: async ({ username, tweetId }) => {
+    return await createImage({ username, tweetId, makeFile })
+  }})
 })
